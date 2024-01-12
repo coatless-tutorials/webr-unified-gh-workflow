63c368037eb3f8223d23fd31db9ac8f5e2169082
James J Balamuta <james.balamuta@gmail.com>
2024-01-11 16:23:24 -0800

    Export function and switch name to `in_webr()`

--- R/is-wasm.R
+++ R/is-wasm.R
@@ -4,7 +4,8 @@
 #' that matches `"wasm32"`. If its the case, we're likely using webR to access
 #' R.
 #' 
+#' @export
 #' @examples
 #' # Check to see if WASM is active
-#' is_r_using_wasm()
-is_r_using_wasm <- function() { R.Version()$arch == "wasm32"}
+#' in_webr()
+in_webr <- function() { R.Version()$os == "emscripten" }
